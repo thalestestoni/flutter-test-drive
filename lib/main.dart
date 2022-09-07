49d202b6b3a543b4335ca0fb51bbf4e83e63f223
Thales Flores Testoni <thales.testoni@havan.com.br>
2022-09-07 10:06:22 -0300

    Adicionado internacionalização de mensagens

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +13,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'MyApp',
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate
+      ],
+      supportedLocales: const [
+        Locale('en', ''), // English, no country code
+        Locale('pt', 'BR') // Portuguese, Brazil
+      ],
       home: Scaffold(
         appBar: AppBar(
           title: const Text('MyApp'),
@@ -45,20 +57,16 @@ class _HelloWorldWidgetState extends State<HelloWorldWidget> {
         children: [
           TextButton(
             onPressed: _handleClickHelloWorld, 
-            child: const Text(
-              'Hello World!',
-              style: TextStyle(
-                fontSize: 50
-              ),
+            child: Text(
+              AppLocalizations.of(context)!.helloWorld,
+              style: const TextStyle(fontSize: 50),
               ) 
           ),
           SizedBox(
             child: SizedBox(
               child: Text(
                 '$_clickCounter',
-                style: const TextStyle(
-                  fontSize: 30
-                ),
+                style: const TextStyle(fontSize: 30),
               )
             ),
           )
